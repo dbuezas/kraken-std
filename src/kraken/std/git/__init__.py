deb11efdb72d78229157ecd06f6b552d3c75cceb
Luke Baxter <luke.bax@talktalk.net>
2023-03-08 17:36:54 +0000

    Tidy up

--- src/kraken/std/git/__init__.py
+++ src/kraken/std/git/__init__.py
@@ -6,13 +6,12 @@ from typing import Optional, Sequence, cast
 
 from kraken.core.api import Project
 
+from .gitignore import GITIGNORE_TASK_NAME, DEFAULT_GITIGNORE_TOKENS
 from .tasks.gitignore_sync_task import GitignoreSyncTask
 from .tasks.gitignore_check_task import GitignoreCheckTask
 from .version import GitVersion, git_describe
 
 __all__ = ["git_describe", "GitVersion", "GitignoreSyncTask", "gitignore"]
-
-GITIGNORE_TASK_NAME = "gitignore"
 
 
 def gitignore(
@@ -20,10 +19,11 @@ def gitignore(
     trash2,
     *,
     project: Project | None = None,
+    tokens: Sequence[str] = DEFAULT_GITIGNORE_TOKENS,
 ) -> GitignoreSyncTask:
     project = project or Project.current()
     task = cast(Optional[GitignoreSyncTask], project.tasks().get(GITIGNORE_TASK_NAME))
     if task is None:
-        task = project.do(GITIGNORE_TASK_NAME, GitignoreSyncTask, group="apply")
-        project.do(GITIGNORE_TASK_NAME + ".check", GitignoreCheckTask, group="check")
+        task = project.do(GITIGNORE_TASK_NAME, GitignoreSyncTask, group="apply", tokens=tokens)
+        project.do(GITIGNORE_TASK_NAME + ".check", GitignoreCheckTask, group="check", tokens=tokens)
     return task
--- src/kraken/std/git/tasks/gitignore_sync_task.py
+++ src/kraken/std/git/tasks/gitignore_sync_task.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Dict, List, Optional, Sequence
+from typing import Sequence
 
 from kraken.core.api import Project, Property, TaskStatus
 from kraken.core.lib.render_file_task import RenderFileTask
 from kraken.core.lib.check_file_contents_task import as_bytes
 
-from ..gitignore import GitignoreFile, parse_gitignore
+from ..gitignore import GitignoreFile, DEFAULT_GITIGNORE_TOKENS
 
 
 # TODO(david-luke): ch2. The apply fn should save a copy of the replaced file .gitignore.old
@@ -28,29 +28,39 @@ class GitignoreSyncTask(RenderFileTask):
     file: Property[Path]
     sort_paths: Property[bool] = Property.config(default=True)
     sort_groups: Property[bool] = Property.config(default=False)
+    tokens: Property[Sequence[str]] = Property.config(default=DEFAULT_GITIGNORE_TOKENS)
 
     def __init__(self, name: str, project: Project) -> None:
         super().__init__(name, project)
         self.file.setcallable(lambda: self.project.directory / ".gitignore")
         self.content.setcallable(lambda: self.generate_file_contents(self.file.get()))
 
-    def execute(self) -> TaskStatus:
-        file = self.file.get()
-        if file.exists():
-            existing_content = file.read_bytes()
-            new_content = as_bytes(self.content.get(), self.encoding.get())
-            if existing_content != new_content:
-                backup_file = self.project.directory / ".gitignore.old"
-                backup_file.write_bytes(existing_content)
+    def prepare(self) -> TaskStatus | None:
+        try:
+            return super().prepare()
+        except AssertionError as assert_error:
+            return TaskStatus.failed(f"Could not generate to the gitignore file: {assert_error}")
 
-        return super().execute()
+    def execute(self) -> TaskStatus:
+        try:
+            file = self.file.get()
+            if file.exists():
+                existing_content = file.read_bytes()
+                new_content = as_bytes(self.content.get(), self.encoding.get())
+                if existing_content != new_content:
+                    backup_file = self.project.directory / ".gitignore.old"
+                    backup_file.write_bytes(existing_content)
+
+            return super().execute()
+        except AssertionError as assert_error:
+            return TaskStatus.failed(f"Could not generate to the gitignore file: {assert_error}")
 
     def generate_file_contents(self, file: Path) -> str | bytes:
         if file.exists():
-            gitignore = parse_gitignore(file)
+            gitignore = GitignoreFile.parse_file(file)
         else:
             gitignore = GitignoreFile([])
-        gitignore.refresh_generated_content()
-        gitignore.refresh_hash()
+        gitignore.refresh_generated_content(tokens=self.tokens.get())
+        gitignore.refresh_generated_content_hash()
         gitignore.sort_gitignore(self.sort_paths.get(), self.sort_groups.get())
         return gitignore.render()
